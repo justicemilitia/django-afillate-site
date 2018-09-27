1b0066db3c147ef81c7b39301017d8eafb956b0c
Baran Can AYDIN <barancanaydin@gmail.com>
2018-09-28 02:20:56 +0300

    admin page change

--- blog/home/admin.py
+++ blog/home/admin.py
@@ -1,21 +1,42 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Product
+
+
+def get_picture_preview(obj):
+    if obj.pk:  # if object has already been saved and has a primary key, show picture preview
+        return mark_safe("""<a href="{src}" target="_blank"><img src="http://127.0.0.1:5000/static/images/products/{src}" alt="{title}" style="max-width: 200px; max-height: 200px;" /></a>""".format(
+            src=obj.image,
+            title=obj.title,
+        ))
+    return ("(choose a picture and save and continue editing to see the preview)")
+
+
+get_picture_preview.allow_tags = True
+get_picture_preview.short_description = ("Picture Preview")
+
 
 class ProductsAdmin(admin.ModelAdmin):
 
-    list_display=['title','published'] #listeleme alanındaki gösterilecek kolonlar
-    list_display_links=['published'] # listeleme alanındaki link verilen kolonlar
-    list_filter=['published'] # sayfada filtreleme alanı
-    search_fields=['title','content'] #arama alanlı
-    list_editable=['title'] # editable belirlenen alanlar link olarak ayarlanmamalı!!! list filterden title alanını kaldırdım.
-    
-
+    # listeleme alanındaki gösterilecek kolonlar
+    list_display = ['title', 'image', 'price', 'published']
+    # listeleme alanındaki link verilen kolonlar
+    list_display_links = ['published', 'image', 'price']
+    list_filter = ['published']  # sayfada filtreleme alanı
+    search_fields = ['title', 'content']  # arama alanlı
+    # editable belirlenen alanlar link olarak ayarlanmamalı!!! list filterden title alanını kaldırdım.
+    list_editable = ['title']
+    # save_on_top = True
+    fields = ["title", "image", "content", get_picture_preview]
+    readonly_fields = [get_picture_preview]
+    # inlines = [ReviewInline]
 
     class Meta:
-        model =Product
+        model = Product
 
 
 # Register your models here.
-admin.site.register(Product,ProductsAdmin)
+admin.site.register(Product, ProductsAdmin)
 
 # Register your models here.
--- blog/home/models.py
+++ blog/home/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     content = models.TextField(verbose_name='Content')
     title = models.CharField(max_length=500,verbose_name='Title')
     link = models.CharField(max_length=2000,verbose_name='Link')
-    image = models.CharField(max_length=200,verbose_name='Image')
+    image = models.ImageField(null=True,blank=True)
     slug = models.CharField(max_length=200,verbose_name='Slug')
     published = models.DateTimeField(verbose_name='Published Date')
     price = models.CharField(max_length=30,verbose_name='Price')
